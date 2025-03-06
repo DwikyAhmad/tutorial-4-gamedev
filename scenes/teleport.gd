1edef2b06258a2000f0d3a5978c5ae1a55e54839
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2025-03-06 13:14:44 +0700

    gdformat fix

--- scenes/teleport.gd
+++ scenes/teleport.gd
@@ -6,16 +6,16 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    # Set up collision detection
-    monitoring = true
-    monitorable = true
+	# Set up collision detection
+	monitoring = true
+	monitorable = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-    pass
+	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
-    if body.get_name() == "Player":
-        body.global_position = Vector2(teleport_x, teleport_y)
+	if body.get_name() == "Player":
+		body.global_position = Vector2(teleport_x, teleport_y)
